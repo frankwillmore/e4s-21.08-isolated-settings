28937ad642f0bc6d97ff563c44670fc101ec736b
Frank Willmore <willmore@anl.gov>
2021-11-08 16:24:00 +0000

    add intermedate step to fix generated yaml

--- fix_yaml.sh
+++ fix_yaml.sh
@@ -1,1 +1,2 @@
-sed -i -e "s/needs\: \[\]/needs\: \[\{pipeline: \$PARENT_PIPELINE_ID, job\: generate-spack-pipeline\} \]/" jobs_scratch_dir/pipeline.yml 
+sed -i -e "s/needs\: \[\]/needs\: \[\{pipeline: \$PARENT_PIPELINE_ID, job\: generate-spack-pipeline\} \]/" jobs_scratch_dir/_pipeline.yml 
+mv jobs_scratch_dir/_pipeline.yml jobs_scratch_dir/pipeline.yml 
